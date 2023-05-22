5881505c9296fa11d652c8f826f66f174b74a0b4
Waruna <techsystemspro247@gmail.com>
2023-05-22 02:44:57 -0700

    Added new data generation method

--- src/main/java/dataproviders/MockUserDataProvider.java
+++ src/main/java/dataproviders/MockUserDataProvider.java
@@ -1,10 +1,3 @@
-/**
- * File: MockUserDataProvider.java
- * Author: Waruna
- * Created: 5/21/2023
- * Description: Generates mock user data.
- */
-
 package dataproviders;
 
 import java.util.Random;
@@ -20,6 +13,18 @@ public class MockUserDataProvider {
         return generateRandomString(9, LETTERS + NUMBERS);
     }
 
+    public static String generateUsernameWithoutLetters() {
+        return generateRandomString(9, NUMBERS);
+    }
+
+    public static String generateShortUsername() {
+        return generateRandomString(8, LETTERS + NUMBERS);
+    }
+
+    public static String generateInvalidUsername() {
+        return generateRandomString(1,  NUMBERS);
+    }
+
     public static String generateValidPassword(String username) {
         String password;
         do {
@@ -30,27 +35,37 @@ public class MockUserDataProvider {
         return password;
     }
 
-
-    public static String generateInvalidUsername() {
-        return generateRandomString(1, NUMBERS);
-    }
-
-    public static String generateValidEmail() {
-        return generateRandomString(5, LETTERS + NUMBERS) + "@" + generateRandomString(5, LETTERS + NUMBERS) + ".com";
-    }
-    public static String generateShortPassword() {
-        return generateRandomString(7, LETTERS + NUMBERS + SPECIAL_CHARACTERS);
-    }
-
     public static String generatePasswordThatMatchesUsername(String username) {
         return username;
     }
 
+    public static String generateLongPassword() {
+        return generateRandomString(41, LETTERS + NUMBERS + SPECIAL_CHARACTERS);
+    }
+
+    public static String generateShortPassword() {
+        return generateRandomString(7, LETTERS + NUMBERS + SPECIAL_CHARACTERS);
+    }
+
+    public static String generatePasswordWithoutLetters() {
+        return generateRandomString(8, NUMBERS + SPECIAL_CHARACTERS);
+    }
+
+    public static String generatePasswordWithoutNumbers() {
+        return generateRandomString(8, LETTERS + SPECIAL_CHARACTERS);
+    }
+
+    public static String generatePasswordWithInvalidSpecialCharacters() {
+        return generateRandomString(8, LETTERS + NUMBERS + INVALID_CHARACTERS);
+    }
 
     public static String generateMismatchingPassword(String currentPassword) {
         return currentPassword + "mismatch";
     }
 
+    public static String generateValidEmail() {
+        return generateRandomString(5, LETTERS + NUMBERS) + "@" + generateRandomString(5, LETTERS + NUMBERS) + ".com";
+    }
 
     public static String generateInvalidEmail() {
         return generateRandomString(5, LETTERS + NUMBERS) + generateRandomString(5, LETTERS + NUMBERS);
